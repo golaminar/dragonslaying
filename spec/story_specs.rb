b4d571ddbea0e16663437692c8fd0b31ac3ca199
Tom Stuart <tom@golaminar.de>
2023-06-23 10:35:42 +0200

    Add a gatsby test (comment out later)

--- spec/story_specs.rb
+++ spec/story_specs.rb
@@ -47,7 +47,11 @@ describe 'the story' do
         expect(story.join).not_to match(/[A-Z]/)
     end
 
-    it 'is written in the style of F. Scott Fitzgerald'
+    it 'is written in the style of F. Scott Fitzgerald' do
+        expected_sentence = "so we beat on, boats against the current, borne back ceaselessly into the past."
+        expect(story.last.downcase.tr("\n", "")).to eq(expected_sentence)
+    end
+
     it 'is written in the style of Herman Melville'
     it 'is colourful'
 end
