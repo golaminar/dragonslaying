8cd08d7b071961a2e17b9beb333366409519f756
Tiffany Conroy <tiffany@soundcloud.com>
2023-09-15 12:25:44 +0200

    mentioned a colour each chapter

--- spec/story_specs.rb
+++ spec/story_specs.rb
@@ -61,7 +61,7 @@ describe 'the story' do
         expect(chapters.first.split("\n")[2].downcase.tr("\n", "")).to eq(expected_sentence)
     end
 
-#   it 'is colourful' do
-#       expect(chapters).to all(match(/(red|green|blue)/i))
-#   end
+   it 'is colourful' do
+       expect(chapters).to all(match(/(red|green|blue)/i))
+   end
 end
